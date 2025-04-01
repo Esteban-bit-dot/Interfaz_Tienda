deb7eabb203ef3a1405ee0fef807dee7c44bb03a
Esteban-bit-dot <eandrescardales@gmail.com>
2025-04-01 09:14:12 -0500

    Create Tienda_Ropa.java

---
+++ Tienda_Ropa.java
@@ -0,0 +1,12 @@
+package gui;
+
+import Vista.Registro_Producto;
+public class Tienda_ropa {
+
+    
+    public static void main(String[] args) {
+        Registro_Productos.objRegistro_Productos new Registro_Productos;
+        objRegistro_Productos.setVisible(b:true);
+    }
+    
+}
